32fe33305fec66f59cd3e3c049857bcab44bb3df
nkleinmann <nekleinmann@gmail.com>
2020-07-25 13:36:21 -0400

    topic variable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    let topic = 
+    let topic = $(".topic").val();
 
     let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic +
     "&api-key=qS1KrSPDOGp7zKrIs176AiGWJAD6EG0k";

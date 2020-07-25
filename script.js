d2c6c0b75e4bac095c786bb1e6db16f5ed2f9d42
nkleinmann <nekleinmann@gmail.com>
2020-07-25 13:37:23 -0400

    topic

--- script.js
+++ script.js
@@ -11,11 +11,3 @@ $(function() {
         }) 
 });
 
-// // Constructing a queryURL using the animal name
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-// animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-// // Performing an AJAX request with the queryURL
-// $.ajax({
-// url: queryURL,
-// method: "GET"
-// })
